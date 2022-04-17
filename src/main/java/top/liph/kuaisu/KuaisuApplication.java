660975ad1aaf85db448d2ddb6042b5e97ee7ee0f
lipenghuihenu <493284461@qq.com>
2022-04-17 21:06:39 +0800

    add

---
+++ src/main/java/top/liph/kuaisu/KuaisuApplication.java
@@ -0,0 +1,13 @@
+package top.liph.kuaisu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuaisuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KuaisuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/liph/kuaisu/KuaisuApplicationTests.java
@@ -0,0 +1,13 @@
+package top.liph.kuaisu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KuaisuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
